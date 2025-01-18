a9330f469d86ed95c86fe0c5321a93815155ab70
Aditya Ghosh <adityaghosh2002@gmail.com>
2025-01-18 19:58:43 +0800

    remove mongo

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'src/settings/settings_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await MongoDatabase.connect();
 
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
@@ -18,8 +17,6 @@ void main() async {
   // Load the user's preferred theme while the splash screen is displayed.
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
-
-
 
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
